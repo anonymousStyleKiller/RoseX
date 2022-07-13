29284cdf74796cfdfcfcab06b0dd9e357c04cb31
Anton <gumb1t97@gmail.com>
2022-07-14 01:01:03 +0300

    Added Middleware for catching  exception

---
+++ Server/RoseX.Server/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace RoseX.Server.Extensions;
+
+internal static class ApplicationBuilderExtensions
+{
+    internal static void UseExceptionHandling(this IApplicationBuilder app, IWebHostEnvironment  environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            app.UseExceptionHandler();
+            app.UseDeveloperExceptionPage();
+        }
+    }
+}
